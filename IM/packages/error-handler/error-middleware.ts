73e8b0e354f016218197d707b2661afdb43f2992
infinitemarket3 <infinitemarket30@gmail.com>
2025-09-24 08:04:37 -0700

    0

--- IM/packages/error-handler/error-middleware.ts
+++ IM/packages/error-handler/error-middleware.ts
@@ -1,8 +1,8 @@
 import {AppError} from "./index";
-import { Request, Response} from "express";
+import { NextFunction, Request, Response} from "express";
 
 
-export const errorMiddleware = (err: Error,req:Request,res:Response) => {
+export const errorMiddleware = (err: Error,req:Request,res:Response,next:NextFunction) => {
     if(err instanceof AppError){
         console.log(`Error ${req.method} ${req.url} - ${err.message}`);
 
