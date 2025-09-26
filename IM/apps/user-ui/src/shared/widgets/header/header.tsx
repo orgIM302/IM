c9037e944459b023cc4c5f52c62dd145cbb234ff
infinitemarket3 <infinitemarket30@gmail.com>
2025-09-25 18:14:22 -0700

    Update header.tsx

--- IM/apps/user-ui/src/shared/widgets/header/header.tsx
+++ IM/apps/user-ui/src/shared/widgets/header/header.tsx
@@ -11,14 +11,19 @@ const Header = () => {
                     <span className="text-2xl font-600 text-green-500">Infinite_Market</span>
                     </Link>
                 </div>
-           </div>
            <div className="w-[50%] relative">
-            <input type="text" placeholder="Search for products..."
+            <input 
+            type="text" 
+            placeholder="Search for products..."
             className="w-full px-4 font-Poppins font-medium border-[2.5px] border-[#3489FF] outline-none h-[55px]"
             />
             <div className="w-[60px] cursor-pointer flex items-center justify-center h-[55px] bg-[#3489FF] absolute top-0 right-0"/>
-            <Search color="#fff" />
-           </div>
+              <Search />
+            </div>
+            <div className="flex items-center gap-8">
+
+            </div>
+        </div>
         </div>
     );
 };
